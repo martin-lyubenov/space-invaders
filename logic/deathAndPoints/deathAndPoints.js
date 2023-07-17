e64ffc0f1312be2caf788bfce40abe7a8622ea12
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-17 16:08:30 +0300

    deathAndPoints moved to module

---
+++ logic/deathAndPoints/deathAndPoints.js
@@ -0,0 +1,27 @@
+export function deathAndPoints(aliens, lazerShots, isCollision, sounds, scene) {
+  aliens.forEach((alien) => {
+    lazerShots.forEach((lazerShot) => {
+      if (
+        isCollision(lazerShot, alien) &&
+        alien.classList.contains("dead-alien") === false
+      ) {
+        alien.style.backgroundImage =
+          "url('./images/aliens/death-animation.jpg')";
+        setTimeout(() => {
+          alien.classList.add("dead-alien");
+        }, 50);
+        lazerShot.remove();
+        sounds.alienDeathSound.play();
+        if (alien.classList.contains("alien-40pts")) {
+          scene.score += 40;
+        } else if (alien.classList.contains("alien-20pts")) {
+          scene.score += 20;
+        } else if (alien.classList.contains("alien-10pts")) {
+          scene.score += 10;
+        } else if (alien.classList.contains("mothership")) {
+          scene.score += 300;
+        }
+      }
+    });
+  });
+}
