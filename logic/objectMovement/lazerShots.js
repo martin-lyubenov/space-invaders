6ee6aa345cb09f52108f469c2b4725eb45f4c48f
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-17 16:08:05 +0300

    lazerShorts movement logic moved to module

---
+++ logic/objectMovement/lazerShots.js
@@ -0,0 +1,21 @@
+export function lazerShotsMovement(lazerShots, game, isCollision) {
+  lazerShots.forEach((lazerShot) => {
+    lazerShot.y -= game.speed;
+    lazerShot.style.top = lazerShot.y + "px";
+
+    if (lazerShot.y + lazerShot.offsetHeight < 0) {
+      lazerShot.remove();
+    }
+
+    // if the lazer shot hits a shield, the shot is removed
+    let shields = document.querySelectorAll(".shield");
+    shields.forEach((shield) => {
+      if (
+        isCollision(lazerShot, shield) &&
+        shield.classList.contains("shield-destroyed") === false
+      ) {
+        lazerShot.remove();
+      }
+    });
+  });
+}
