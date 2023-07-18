847ecf7e01ca3cb83bb78379d7f1a628dac9b496
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-18 12:27:40 +0300

    alien lazer shot collision checker move to module

---
+++ logic/collisionChecker/defenderAlienLazerShot.js
@@ -0,0 +1,23 @@
+export function defenderAlienLazerShotCollisionChecker(
+  defender,
+  isCollision,
+  alienLazerShot,
+  gameOverAction,
+  player,
+  gameArea
+) {
+  if (isCollision(defender, alienLazerShot)) {
+    let lives = document.querySelectorAll(".one-live");
+    alienLazerShot.remove();
+    if (lives.length > 1) {
+      player.x = 100;
+      player.y = gameArea.offsetHeight - 50;
+      defender.style.left = player.x + "px";
+      defender.style.top = player.y + "px";
+      lives[lives.length - 1].remove();
+    } else {
+      lives[lives.length - 1].remove();
+      gameOverAction();
+    }
+  }
+}
