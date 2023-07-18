4241233d55b04a8262ec497ee3fa5a007918ad1d
Martin Lyubenov <martindlyubenov@gmail.com>
2023-07-18 12:27:53 +0300

    alien lazer shot to shield move to module

---
+++ logic/collisionChecker/shieldAlienLazerShot.js
@@ -0,0 +1,23 @@
+export function shieldAlienLazerShotCollisionChecker(alienLazerShot, isCollision) {
+  let shields = document.querySelectorAll(".shield");
+  shields.forEach((shield) => {
+    if (
+      isCollision(shield, alienLazerShot) &&
+      shield.classList.contains("shield-destroyed") === false
+    ) {
+      if (shield.classList.contains("shield-full-hp")) {
+        shield.classList.remove("shield-full-hp");
+        shield.classList.add("shield-1hit");
+      } else if (shield.classList.contains("shield-1hit")) {
+        shield.classList.remove("shield-1hit");
+        shield.classList.add("shield-2hits");
+      } else if (shield.classList.contains("shield-2hits")) {
+        shield.classList.remove("shield-2hits");
+        shield.classList.add("shield-3hits");
+      } else if (shield.classList.contains("shield-3hits")) {
+        shield.classList.add("shield-destroyed");
+      }
+      alienLazerShot.remove();
+    }
+  });
+}
